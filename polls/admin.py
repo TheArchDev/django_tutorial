56c40c2fb5093eaa6de2ec6d594cf61181a19c1e
Alex Archer <alex@touchsurgery.com>
2016-05-30 19:57:52 +0100

    Django tutorial part 2, admin page stuff

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # inclusion of associated Choice object entries
+    inlines = [ChoiceInline]
+
+    # determines column headers in 'change list' page
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # filter sidebar by publication date
+    list_filter = ['pub_date']
+    # search capability
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -10,8 +11,11 @@ class Question(models.Model):
     def __unicode__(self):
         return self.question_text
 
-    def was_recently_published(self):
+    def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
